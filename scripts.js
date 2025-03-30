fcb2dae4d12875e6859db2259825d9e29dafac2a
Davi Leal <odavileal77@gmail.com>
2025-03-30 14:40:20 -0300

    commit002

--- scripts.js
+++ scripts.js
@@ -3,11 +3,19 @@ let pegaHora = new Date
 let hora = pegaHora.getHours()
 
 if (hora > 6 && hora  < 12){
-    sauda.innerHTML = "Bom Dia <span>Davi</span>"
+    sauda.innerHTML = "Bom Dia <span>Davi</span> ☀️"
 }
 else if (hora > 12 && hora < 19){
-    sauda.innerHTML = "Bom Tarde <span>Davi</span>"
+    sauda.innerHTML = "Boa Tarde <span>Davi</span> ☁️"
 }
 else if (hora > 19 && hora < 23){
-    sauda.innerHTML = "Boa Noite <span>Davi!</span>"
+    sauda.innerHTML = "Boa Noite <span>Davi!</span> 🌜"
+}
+
+function adicionaLista(){
+    let lista = document.getElementById("lista-tarefa")
+    let add_Input = document.getElementById("add-item-input").value
+    alert(add_Input)
+    lista.innerHTML = `<li class="list-group-item">${add_Input}</li>`
+ 
 }
